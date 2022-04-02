0f07a1af7458ed4d31fea3d69aaac698e725f072
henrymeister <enriquemumanrique@gmail.com>
2022-04-02 17:59:03 +0200

    Updated colors

--- app/src/main/java/com/henry/marvelmahle/presentation/comics/ComicsMainFragment.kt
+++ app/src/main/java/com/henry/marvelmahle/presentation/comics/ComicsMainFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,7 +12,6 @@ import com.henry.marvelmahle.data.model.comic.ComicId
 import com.henry.marvelmahle.data.model.comic.ComicResult
 import com.henry.marvelmahle.ext.PaginationScrollListener
 import com.henry.marvelmahle.ext.hideInProgress
-import com.henry.marvelmahle.ext.showInProgress
 import com.henry.marvelmahle.ext.showInProgressTouchable
 import com.henry.marvelmahle.utils.Status
 import kotlinx.android.synthetic.main.character_home_layout.*
